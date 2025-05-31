69ca21a25c37477b4331c4d834a43b147d57c813
Navat2007 <navat2007@yandex.ru>
2025-05-31 11:37:04 +0300

    add

--- src/pages/public/Analytics.vue
+++ src/pages/public/Analytics.vue
@@ -1,5 +1,99 @@
 <script setup>
+import {onMounted, ref} from "vue";
 
+const chartData = ref();
+const chartOptions = ref();
+const chartRadarData = ref();
+const chartRadarOptions = ref();
+
+const setChartData = () => {
+  const documentStyle = getComputedStyle(document.body);
+
+  return {
+    labels: ['A', 'B', 'C'],
+    datasets: [
+      {
+        data: [540, 325, 702],
+        backgroundColor: [documentStyle.getPropertyValue('--p-cyan-500'), documentStyle.getPropertyValue('--p-orange-500'), documentStyle.getPropertyValue('--p-gray-500')],
+        hoverBackgroundColor: [documentStyle.getPropertyValue('--p-cyan-400'), documentStyle.getPropertyValue('--p-orange-400'), documentStyle.getPropertyValue('--p-gray-400')]
+      }
+    ]
+  };
+};
+const setChartOptions = () => {
+  const documentStyle = getComputedStyle(document.documentElement);
+  const textColor = documentStyle.getPropertyValue('--p-text-color');
+
+  return {
+    plugins: {
+      legend: {
+        labels: {
+          usePointStyle: true,
+          color: textColor
+        }
+      }
+    }
+  };
+};
+
+const setRadarChartData = () => {
+  const documentStyle = getComputedStyle(document.documentElement);
+  const textColor = documentStyle.getPropertyValue('--p-text-color');
+
+  return {
+    labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        borderColor: documentStyle.getPropertyValue('--p-gray-400'),
+        pointBackgroundColor: documentStyle.getPropertyValue('--p-gray-400'),
+        pointBorderColor: documentStyle.getPropertyValue('--p-gray-400'),
+        pointHoverBackgroundColor: textColor,
+        pointHoverBorderColor: documentStyle.getPropertyValue('--p-gray-400'),
+        data: [65, 59, 90, 81, 56, 55, 40]
+      },
+      {
+        label: 'My Second dataset',
+        borderColor: documentStyle.getPropertyValue('--p-pink-400'),
+        pointBackgroundColor: documentStyle.getPropertyValue('--p-pink-400'),
+        pointBorderColor: documentStyle.getPropertyValue('--p-pink-400'),
+        pointHoverBackgroundColor: textColor,
+        pointHoverBorderColor: documentStyle.getPropertyValue('--p-pink-400'),
+        data: [28, 48, 40, 19, 96, 27, 100]
+      }
+    ]
+  };
+};
+const setRadarChartOptions = () => {
+  const documentStyle = getComputedStyle(document.documentElement);
+  const textColor = documentStyle.getPropertyValue('--p-text-color');
+  const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+
+  return {
+    plugins: {
+      legend: {
+        labels: {
+          color: textColor
+        }
+      }
+    },
+    scales: {
+      r: {
+        grid: {
+          color: textColorSecondary
+        }
+      }
+    }
+  };
+}
+
+onMounted(() => {
+  chartData.value = setChartData();
+  chartOptions.value = setChartOptions();
+
+  chartRadarData.value = setRadarChartData();
+  chartRadarOptions.value = setRadarChartOptions();
+});
 </script>
 
 <template>
@@ -22,6 +116,14 @@
               <p>Доля мышечной массы: 45.6%</p>
               <p>Доля жировой массы: 18.9%</p>
               <p>Сумма кожно-жировых складок: 70 мм</p>
+
+              <div class="card flex justify-center">
+                <Chart type="pie" :data="chartData" :options="chartOptions" class="w-full md:w-[30rem]" />
+              </div>
+
+              <div class="card flex justify-center">
+                <Chart type="radar" :data="chartRadarData" :options="chartRadarOptions" class="w-full md:w-[30rem]" />
+              </div>
             </template>
           </Card>
         </TabPanel>
